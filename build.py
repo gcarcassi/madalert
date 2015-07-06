ce8d4fa725461bf1356986298f0ecbfef613867f
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2015-07-06 09:35:30 -0400

    Preparing the repo

---
+++ build.py
@@ -0,0 +1,15 @@
+from pybuilder.core import use_plugin, init
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.distutils")
+
+
+name = "madalert"
+default_task = "publish"
+version = "0.1-SNAPSHOT"
+
+@init
+def set_properties(project):
+    pass
