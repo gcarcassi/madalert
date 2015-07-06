79df67cb4af27ef2dca9fa3e2182827e73ae129a
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2015-07-06 13:58:00 -0400

    Matching all site with same status

--- src/main/python/madalert.py
+++ src/main/python/madalert.py
@@ -8,5 +8,26 @@ def retrievegrid(url):
     resp = urllib.urlopen(url)
     return json.load(resp)
 
+def matchTopHalfCell(data, row, column, status):
+    currentStatus = data["grid"][row][column][0]["status"]
+    return status == currentStatus
+
+def matchBottomHalfCell(data, row, column, status):
+    currentStatus = data["grid"][row][column][1]["status"]
+    return status == currentStatus
+
+
+def matchAllSites(data, status):
+    #TODO: check status in range
+    nSites = len(data['columnNames'])
+    for column in range(0, nSites):
+        for row in range(0, nSites):
+            if (column != row):
+                if (not matchTopHalfCell(data, row, column, status) or
+                    not matchTopHalfCell(data, row, column, status)):
+                    return False
+                
+    return True
+
 def matchSiteAllStatus(data, site, status):
     return False
--- src/unittest/python/madalert_matches_tests.py
+++ src/unittest/python/madalert_matches_tests.py
@@ -3,7 +3,15 @@ import unittest
 
 from madalert import *
 
+def loadJSON(path):
+    with open(path) as data_file:
+        return json.load(data_file)
+
+
+
 class HelloWorldTest(unittest.TestCase):
-    def test_matchSiteAllStatus(self):
-        result = matchSiteAllStatus(1,2,3)
-        self.assertEquals(result, False)
+    def test_matchAllSites(self):
+        data = loadJSON("src/unittest/resources/allMissing.json")
+        self.assertEquals(14, len(data["columnNames"]))
+        result = matchAllSites(data, 3)
+        self.assertEquals(result, True)
