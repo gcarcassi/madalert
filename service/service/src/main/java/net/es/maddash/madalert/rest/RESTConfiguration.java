6db26bb441332f2f70a4463e59997203ee4d0f20
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2015-10-06 10:21:34 -0400

    Adding cross origin resource sharing.

--- service/service/src/main/java/net/es/maddash/madalert/rest/RESTConfiguration.java
+++ service/service/src/main/java/net/es/maddash/madalert/rest/RESTConfiguration.java
@@ -29,6 +29,7 @@ public class RESTConfiguration extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(net.es.maddash.madalert.rest.CrossOriginResourceSharingFilter.class);
         resources.add(net.es.maddash.madalert.rest.ReportResource.class);
     }
     
